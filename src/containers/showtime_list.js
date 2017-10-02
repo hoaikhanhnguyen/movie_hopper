510f176862ad28f952db48f223fe37a6a2952a81
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-10-01 23:06:02 -0700

    check recent version

--- src/containers/showtime_list.js
+++ src/containers/showtime_list.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class ShowTimeList extends Component {
     render(){
         return (
-            <div>SHOWTIMES</div>
+            <div>SHOW TIMES</div>
         )
     }
 }
