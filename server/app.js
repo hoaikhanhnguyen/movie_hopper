441cdc8e017238b2ed754f7e5c740defd30e55cc
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-10-16 16:00:59 -0700

    fixed schedule time = converted military to standard

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 const request = require('request');
 const bodyParser = require('body-parser');
 const path = require('path');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
