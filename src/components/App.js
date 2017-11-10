6445b47159087245a1e80560c3e439bb23909de1
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-11-09 18:06:13 -0800

    installed material-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import SearchBar from '../containers/search_bar.js'
 import TheaterList from '../containers/theater_list';
 import ShowTimeList from '../containers/showtime_list';
@@ -7,14 +9,16 @@ import '../App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to Movie Hopper</h2>
-            <SearchBar />
-        </div>
-          <TheaterList />
-          <ShowTimeList />
-      </div>
+        <MuiThemeProvider>
+          <div className="App">
+            <div className="App-header">
+              <h2>Welcome to Movie Hopper</h2>
+                <SearchBar />
+            </div>
+              <TheaterList />
+              <ShowTimeList />
+          </div>
+        </MuiThemeProvider>
     );
   }
 }
